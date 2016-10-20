85733b67a50da3240f1022bb65f4ab746a145b40
Timothy Crane <mickeymouth82@gmail.com>
2016-10-20 15:09:02 -0500

    sort passing tests

--- Prep/problems/sortAndSearch/prompts/sort.js
+++ Prep/problems/sortAndSearch/prompts/sort.js
@@ -8,6 +8,37 @@
 
 const sort = numbers => {
   // TODO: Implement sort here
+  var sorted = true;
+  
+  for (i = 1; i < numbers.length; i++) {
+    if (numbers[i] < numbers[i-1]) {
+      sorted = false;
+    }
+  }
+  
+  if (numbers.length === 0 || sorted === true) {
+    return numbers;
+  }
+  return quickSort(numbers);
 }
 
 module.exports = { sort };
+
+function quickSort(arr) {
+  
+    if (arr.length === 0) {
+    return [];
+  }
+    var pivot = arr.splice([Math.floor(arr.length/2)], 1);
+    var less = [];
+    var more = [];
+  
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] <= pivot) {
+          less.push(arr[i]);
+        } else if (arr[i] > pivot) {
+          more.push(arr[i]);
+        }
+      }
+      return quickSort(less).concat(pivot, quickSort(more));
+}
